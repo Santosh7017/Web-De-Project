21c2b615bb9a66b1590cc510d959eb32a8992df2
vaibhav0726 <vaibhav.varshney2_cs20@gla.ac.in>
2022-07-03 17:42:39 +0530

    basic structure

---
+++ JS/script.js
@@ -0,0 +1,29 @@
+if ($('.first-txt').length) {
+    $('.first-txt').appear(function() {
+
+        var $t = $(this),
+            n = $t.find(".count-text").attr("data-stop"),
+            r = parseInt($t.find(".count-text").attr("data-speed"), 10);
+
+        if (!$t.hasClass("counted")) {
+            $t.addClass("counted");
+            $({
+                countNum: $t.find(".count-text").text()
+            }).animate({
+                countNum: n
+            }, {
+                duration: r,
+                easing: "linear",
+                step: function() {
+                    $t.find(".count-text").text(Math.floor(this.countNum));
+                },
+                complete: function() {
+                    $t.find(".count-text").text(this.countNum);
+                }
+            });
+        }
+
+    }, {
+        accY: 0
+    });
+}
